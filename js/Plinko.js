721778af5b190d9ad12c3886f006f6133cbd8856
Aanvig <78943357+Aanvig@users.noreply.github.com>
2021-07-01 20:02:06 -0400

    Add files via upload

---
+++ js/Plinko.js
@@ -0,0 +1,30 @@
+class Plinko {
+    constructor(x, y) {
+        var options = {
+            'restitution': 0.3,
+            'friction': 0.5,
+            'isStatic':true,
+            'density':1.2
+        }
+        this.r = 10;
+        this.body = Bodies.circle(x, y, this.r, options);
+        
+        World.add(world, this.body);
+    }
+    display() {
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        noStroke();
+        fill("white");
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.r,this.r);
+        pop();
+    }
+
+};
